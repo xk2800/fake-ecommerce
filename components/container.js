3a2ece1baf42180f3ded809cde55682146acf0f6
Xavier K <xavierkhew00@gmail.com>
2021-09-12 13:50:22 +0800

    add: contents needed for individual items

--- components/container.js
+++ components/container.js
@@ -1,7 +1,9 @@
 export default function container({ children }) {
     return (
-        <div className="mx-auto mx-w-xl w-[80%]">
-            {children}
+        <div className="bg-gray-200">
+            <div className="mx-auto mx-w-xl w-[80%] ">
+                {children}
+            </div>
         </div>
     );
 }
--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export const getStaticPaths = async () => {
     const res = await fetch("https://fakestoreapi.com/products");
     const data = await res.json();
@@ -31,8 +33,32 @@ export const getStaticProps = async (context) => {
 
 export default function Details({ products }) {
     return (
-        <div>
-            <p>{products.title}</p>
+        <div className="h-screen bg-green-800">
+            <br />
+            <div className="bg-white grid grid-cols-2">
+                <Image src={products.image}
+                    alt={products.title}
+                    width={150}
+                    height={150}
+                    className="object-contain"
+                />
+                <div>
+                    <p>{products.title}</p>
+                    Rating | number of ratings | amount sold <br />
+                    <p>RM{products.price}</p>
+                    Shipping <br />
+                    <br />
+                    ---Add to Cart --- ---buy now---
+                </div>
+            </div>
+            <br />
+            ---Prod Specs--- <br />
+            <p>{products.category}</p>
+            ---Prod Description---<br />
+            <p>{products.description}</p>
+
+
+
         </div>
     );
 }
